e52d5f5ff1b9b1006e67cc819737408169f57df2
pon-droid <tellepsarecool@gmail.com>
2021-11-20 22:49:05 +1100

    Added input and board mutation

--- main.cpp
+++ main.cpp
@@ -16,19 +16,36 @@ void print_board(char board[B][B]){
    std::cout << 0 << " |" << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << " | " << std::endl;
    std::cout << 1 << " |" << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << " | " << std::endl;
    std::cout << 2 << " |" << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << " | " << std::endl;
-
-
-
-
 }
+
+void input(char board[B][B],char play){
+    int x,y;
+    std::cout << "Enter Column: " << std::endl;
+    std::cin >> x;
+    std::cout << "Enter Row: " << std::endl;
+    std::cin >> y;
+
+    if(play == 'O'){
+        play = 'X';
+    } else{
+        play = 'O';
+    }
+
+    board[y][x] = play;
+}
+
 
 int main()
 {
     char board[B][B];
     board_init(board);
-    board[1][2] = 88;
-    board[2][1] = 79;
-    print_board(board);
+
+    bool win = false;
+    char play = 'O';
+    while(!win){
+        print_board(board);
+        input(board, play);
+    }
 
     return 0;
 }
