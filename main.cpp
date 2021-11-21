799929a4a412b21b1e22f5fcf22dcf6e20ad4159
pon-droid <tellepsarecool@gmail.com>
2021-11-21 13:03:59 +1100

    Win checking

--- main.cpp
+++ main.cpp
@@ -18,34 +18,67 @@ void print_board(char board[B][B]){
    std::cout << 2 << " |" << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << " | " << std::endl;
 }
 
-void input(char board[B][B],char play){
+void input(char board[B][B],char *play){
     int x,y;
     std::cout << "Enter Column: " << std::endl;
     std::cin >> x;
     std::cout << "Enter Row: " << std::endl;
     std::cin >> y;
 
-    if(play == 'O'){
-        play = 'X';
+    if(*play == 'O'){
+        *play = 'X';
     } else{
-        play = 'O';
+        *play = 'O';
     }
 
-    board[y][x] = play;
+    board[y][x] = *play;
 }
 
+bool win(char board[B][B]){
+    //rows and columns
+    for(int i = 0; i < B; i++){
+        //rows
+
+        if(board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] != 32){
+            return true;
+        }
+
+        //Cols
+
+        if(board[0][i] == board[1][i] && board[0][i] == board[2][i] && board[0][i] != 32){
+            return true;
+        }
+
+    }
+
+    if(board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != 32){
+        return true;
+    }
+
+    if(board[2][0] == board[1][1] && board[2][0] == board[0][2] && board[2][0] != 32){
+        return true;
+    }
+
+    return false;
+}
 
 int main()
 {
     char board[B][B];
     board_init(board);
 
-    bool win = false;
+
     char play = 'O';
-    while(!win){
+    while(true){
         print_board(board);
-        input(board, play);
+        input(board, &play);
+        if(win(board) == true){
+            print_board(board);
+            break;
+        }
     }
+
+    std::cout << play << " Has Won!" << std::endl;
 
     return 0;
 }
